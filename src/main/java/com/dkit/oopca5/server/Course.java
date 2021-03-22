fac18c6730f0f2eb894201fb0ffccbc5bd199ce2
Brian McKenna <D00197352@student.dkit.ie>
2021-03-22 17:19:24 +0000

    Course POJO Created

---
+++ src/main/java/com/dkit/oopca5/server/Course.java
@@ -0,0 +1,73 @@
+package com.dkit.oopca5.server;
+
+public class Course
+{
+    private String courseid;
+    private int level;
+    private String title;
+    private String institution;
+
+
+    public Course(String courseid, int level, String title, String institution)
+    {
+        this.courseid = courseid;
+        this.level = level;
+        this.title = title;
+        this.institution = institution;
+    }
+
+    public Course()
+    {
+    }
+
+    public String getCourseid()
+    {
+        return courseid;
+    }
+
+    public void setCourseid(String courseid)
+    {
+        this.courseid = courseid;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public void setLevel(int level)
+    {
+        this.level = level;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getInstitution()
+    {
+        return institution;
+    }
+
+    public void setInstitution(String institution)
+    {
+        this.institution = institution;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Course{" +
+                "courseid='" + courseid + '\'' +
+                ", level=" + level +
+                ", title='" + title + '\'' +
+                ", institution='" + institution + '\'' +
+                '}';
+    }
+}
