b80d9fafb7661dd50ace421c97b8931c884925ff
Brian McKenna <D00197352@student.dkit.ie>
2021-04-01 17:43:58 +0100

    JUNIT tests for client requests complete

--- src/test/java/com/dkit/oopca5/core/test/AppTest.java
+++ src/test/java/com/dkit/oopca5/core/test/AppTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -49,6 +50,35 @@ public class AppTest
         System.out.println("Testing Regex Course ID");
         String courseID = "DK821";
         assertTrue(isValidCourseId(courseID, CAOService.COURSE_ID_REGEX));
+    }
+
+    @Test
+    public void TestDisplayCourseClientRequest()
+    {
+        System.out.println("Testing Display Course Client Request");
+        String courseID = "DK821";
+        String expectedResult = "DISPLAY COURSE%%DK821";
+        String actualResult = CAOService.DISPLAY_COURSE_COMMAND + CAOService.BREAKING_CHARACTER + courseID;
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void TestDisplayCoursesClientRequest()
+    {
+        System.out.println("Testing Display All Courses Client Request");
+        String expectedResult = "DISPLAY_ALL";
+        String actualResult = CAOService.DISPLAY_ALL_COURSES_COMMAND;
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void TestDisplayCurrentCoursesClientRequest()
+    {
+        System.out.println("Testing Display Current Courses Client Request");
+        int caoNumber = 12345678;
+        String expectedResult = "DISPLAY CURRENT%%12345678";
+        String actualResult = CAOService.DISPLAY_CURRENT_COMMAND + CAOService.BREAKING_CHARACTER + caoNumber;
+        assertEquals(expectedResult,actualResult);
     }
 
     public static boolean isValidCAONumber(String caoNumber, String caoNumberRegex)
